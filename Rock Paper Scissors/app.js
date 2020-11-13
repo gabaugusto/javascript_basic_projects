feb36bd3ec99d9b8c456c325de9ed25dae61c609
Gabriel Augusto <33496735+gabaugusto@users.noreply.github.com>
2020-11-12 21:45:59 -0300

    Update app.js

--- Rock Paper Scissors/app.js
+++ Rock Paper Scissors/app.js
@@ -1,47 +1,47 @@
 let userChosen
 let computerChosen
-const displayResult = document.getElementById('result')
-const userChoice = document.getElementById('user-choice')
-var result = results()
-const possibleChoices = document.querySelectorAll('.choices')
-const computerChoice = document.getElementById('computer-choice')
-const randomNumber = Math.round(Math.random() * (3))
+const displayResult = document.getElementById('result');
+const userChoice = document.getElementById('user-choice');
+var result = results();
+const possibleChoices = document.querySelectorAll('.choices');
+const computerChoice = document.getElementById('computer-choice');
+const randomNumber = Math.round(Math.random() * (3));
 
 // Get users userChoice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChosen = e.target.id
-  generatedComputerChoice()
-  results()
-  userChoice.innerHTML = userChosen
-  computerChoice.innerHTML = computerChosen
-  displayResult.innerHTML = result
+  userChosen = e.target.id;
+  generatedComputerChoice();
+  results();
+  userChoice.innerHTML = userChosen;
+  computerChoice.innerHTML = computerChosen;
+  displayResult.innerHTML = result;
 }))
 
 //Get a random computers choice
 function generatedComputerChoice() {
   if (randomNumber === 1) {
-    return computerChosen = 'rock'
+    return computerChosen = 'rock';
   } else if (randomNumber === 2) {
-    return computerChosen = 'paper'
+    return computerChosen = 'paper';
   } else if (randomNumber === 3) {
-    return computerChosen = 'scissors'
+    return computerChosen = 'scissors';
   }
 }
 
 function results() {
   if (computerChosen == userChosen) {
-    return result = 'There was a tie.'
+    return result = 'There was a tie!';
   } else if (computerChosen === 'rock' && userChosen === 'paper') {
-    return result = 'You lost!'
+    return result = 'You lost!';
   } else if (computerChosen === 'rock' && userChosen === 'scissors') {
-    return result = 'You win!'
+    return result = 'You win!';
   } else if (computerChosen === 'paper' && userChosen === 'rock') {
-    return result = 'You lost!'
+    return result = 'You lost!';
   } else if (computerChosen === 'paper' && userChosen === 'scissors') {
-    return result = 'You win!'
+    return result = 'You win!';
   } else if (computerChosen === 'scissors' && userChosen === 'rock') {
-    return result = 'You win!'
+    return result = 'You win!';
   } else if (computerChosen === 'scissors' && userChosen === 'paper') {
-    return result = 'You lost!'
+    return result = 'You lost!';
   }
 }
